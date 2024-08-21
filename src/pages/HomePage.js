a0340960c90ea62840b8729757a813cb30d0c8b0
Sebastian Raju <sebzstianraju79@gmail.com>
2024-08-21 11:20:10 +0530

    small change in padding

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -202,7 +202,7 @@ function HomePage() {
       </div>
   
   
-      <div className="main-hero-section px-[20px] vsm:px-[50px] pb-[50px] pt-[50px] vsm:pt-[70px] mlg:pt-[30px] w-[100%] flex flex-col items-center justify-center relative">
+      <div className="main-hero-section px-[20px] sm:px-[50px] pb-[50px] pt-[50px] vsm:pt-[70px] mlg:pt-[30px] w-[100%] flex flex-col items-center justify-center relative">
         <div className="w-[100%] max-w-container grid grid-cols-12 items-center">
           <div className="col-span-12 mlg:col-span-8 translate-y-[-10px]">
             <div className="w-[100%] grid grid-cols-12 items-end gap-[25px] md:gap-[50px] overflow-hidden">
@@ -807,28 +807,28 @@ function HomePage() {
       <div className="w-[100%] bg-[#FFFFFF] flex justify-center pt-[50px] pb-[50px] px-[24px] vsm:px-[50px]">
         <div className="w-[100%] max-w-container ">
           <div className="grid grid-cols-12 items-center gap-y-[20px] mlg:gap-[0px]">
-            <div className="pe-[0px] py-[15px] border-r-0  vsm:pe-[30px] vsm:py-[0px] col-span-12 vsm:col-span-6 mlg:col-span-3 vsm:border-r border-black flex items-center gap-[20px] justify-center overflow-hidden" data-aos="fade-left" data-aos-duration="300">
+            <div className="pe-[0px] py-[15px] border-r-0  vsm:pe-[30px] vsm:py-[0px] col-span-12 vsm:col-span-6 mlg:col-span-3 vsm:border-r border-black flex items-center gap-[20px] justify-center overflow-hidden" data-aos="fade-left" data-aos-duration="700">
               <img src="./assets/icons/reward.svg" className="w-[40px] h-[40px]" alt=""/>
               <div className="min-w-[107px]">
                 <div className="text-[14px] mb-[5px]">Reward Program</div>
                 <div className="text-[10px]">Morbi tristique felis.</div>
               </div>
             </div>
-            <div className="px-[0px] py-[15px] vsm:ps-[30px] mlg:px-[30px] col-span-12 vsm:col-span-6 mlg:col-span-3 border-r-0 mlg:border-r border-black flex items-center justify-center gap-[20px] overflow-hidden" data-aos="fade-left" data-aos-duration="300">
+            <div className="px-[0px] py-[15px] vsm:ps-[30px] mlg:px-[30px] col-span-12 vsm:col-span-6 mlg:col-span-3 border-r-0 mlg:border-r border-black flex items-center justify-center gap-[20px] overflow-hidden" data-aos="fade-left" data-aos-duration="700">
               <img src="./assets/icons/discount.svg" className="w-[40px] h-[40px]" alt="" />
               <div className="min-w-[107px]">
                 <div className="text-[14px] mb-[5px]">Special Discounts</div>
                 <div className="text-[10px]">Morbi tristique felis.</div>
               </div>
             </div>
-            <div className=" vsm:pe-[30px] px-[0px] py-[15px] mlg:px-[30px] col-span-12 vsm:col-span-6 mlg:col-span-3 border-r-0 vsm:border-r border-black flex items-center justify-center gap-[20px] overflow-hidden" data-aos="fade-left" data-aos-duration="300">
+            <div className=" vsm:pe-[30px] px-[0px] py-[15px] mlg:px-[30px] col-span-12 vsm:col-span-6 mlg:col-span-3 border-r-0 vsm:border-r border-black flex items-center justify-center gap-[20px] overflow-hidden" data-aos="fade-left" data-aos-duration="700">
               <img src="./assets/icons/shipping.svg" className="w-[50px] h-[40px]" alt=""/>
               <div className="min-w-[107px]">
                 <div className="text-[14px] mb-[5px]">Fast shipping</div>
                 <div className="text-[10px]">Morbi tristique felis.</div>
               </div>
             </div>
-            <div className="px-[0px] py-[15px] vsm:ps-[30px] col-span-12 vsm:col-span-6 mlg:col-span-3 flex items-center gap-[20px] justify-center overflow-hidden" data-aos="fade-left" data-aos-duration="300">
+            <div className="px-[0px] py-[15px] vsm:ps-[30px] col-span-12 vsm:col-span-6 mlg:col-span-3 flex items-center gap-[20px] justify-center overflow-hidden" data-aos="fade-left" data-aos-duration="700">
               <img src="./assets/icons/greatPrices.svg" className="w-[40px] h-[40px]" alt=""/>
               <div className="min-w-[107px]">
                 <div className="text-[14px] mb-[5px]">Great prices</div>
